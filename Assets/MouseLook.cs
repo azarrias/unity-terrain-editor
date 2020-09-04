1d38fa929bd33ba6dbb7977486fa427e33e4bf40
azarrias <adolfo.zarrias@gmail.com>
2020-09-04 10:37:28 +0200

    Allow the player to rotate also on the x axis

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -6,11 +6,13 @@ public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
     public Transform playerBody;
+    float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Lock cursor to the center of the screen and hide it
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -19,6 +21,13 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        // Keep rotation on the X axis between -90 and 90 degrees
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate camera on the x axis
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        // rotate whole player body on the y axis
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
